6e291385ffbb3837b0a432fc724d7afcda729eba
LayMui <laymui.toh@taiger.com>
2021-02-01 18:56:23 +0800

    Add youtube link

--- src/views/pages/Promotion.vue
+++ src/views/pages/Promotion.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="text-center">
     <img  contain height=500px width=900px src="../../assets/image/promotion.png"/>
+      <div>
+      <a href="soygreen-sharing" target="_blank">https://www.youtube.com/watch?v=Ql9RZIWJ1WI</a>
   </div>
+  </div>
+
+  
 </template>
