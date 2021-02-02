efef888f916159ccfdb3835a074f64840d41f7aa
LayMui <laymui.toh@taiger.com>
2021-02-03 06:32:41 +0800

    Fix aref youtube link

--- src/views/pages/Promotion.vue
+++ src/views/pages/Promotion.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="text-center">
     <img  contain height=auto width=100%  src="../../assets/image/promotion.png"/>
-      <div>
-      <a href="soygreen-sharing" target="_blank">https://www.youtube.com/watch?v=Ql9RZIWJ1WI</a>
-    </div>
+     <p class="text-muted text-center">
+      Watch Youtube Channel <a href="https://www.youtube.com/watch?v=Ql9RZIWJ1WI" target="_blank">https://www.youtube.com/watch?v=Ql9RZIWJ1WI</a>
+    </p>
   </div>
 
   
